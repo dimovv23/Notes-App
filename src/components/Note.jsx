8244a8e03d46ffa1ea42bc60145d50074814b6af
dimovv23 <ggdimovv@gmail.com>
2024-02-09 19:39:04 +0200

    Made the "Save Note" feature

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,20 +1,55 @@
 import { MdDeleteSweep } from "react-icons/md";
+import { useRef } from "react";
 
-export default function Note() {
+export default function Note({ content, input = false, onAdd }) {
+  const userInput = useRef();
+
+  function handleSave() {
+    const userContent = userInput.current.value;
+
+    if (userContent.trim() === "") {
+      //TODO: Modal for error message
+      return;
+    }
+
+    onAdd({
+      noteContent: userContent,
+    });
+
+    userInput.current.value = "";
+  }
+
   return (
-    <div className="note">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec diam
-        iaculis, laoreet massa in, feugiat mauris. Nulla facilisi. Vestibulum
-        commodo eleifend ante sed auctor. Sed nisl magna, rutrum eget ante
-        vitae, consequat ultricies augue. Donec ultrices ultrices mauris vitae.
-      </p>
-      <div className="note-footer">
-        <span>//date</span>
-        <button>
-          <MdDeleteSweep className="delete-icon" size="1.3rem" />
-        </button>
-      </div>
+    <div>
+      {!input && (
+        <div className="note">
+          {" "}
+          <p>{content}</p>
+          <div className="note-footer">
+            <span>//date</span>
+            <button>
+              <MdDeleteSweep className="delete-icon" size="1.3rem" />
+            </button>
+          </div>
+        </div>
+      )}
+      {input && (
+        <div className={input ? "new-note" : "note"}>
+          {" "}
+          <textarea
+            ref={userInput}
+            name=""
+            id=""
+            cols="32"
+            rows="10"
+            placeholder="Start typping to add a note..."
+          ></textarea>
+          <div className="note-footer">
+            <span>//brChar</span>
+            <button onClick={handleSave}>Save</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import Note from "./Note";
 
 export default function NotesList() {
+  const [notesState, setNotesState] = useState({
+    notes: [],
+  });
+
+  function handleAddNote(noteData) {
+    setNotesState((prevState) => {
+      const noteId = Math.random();
+      const newNote = {
+        ...noteData,
+        id: noteId,
+      };
+      return {
+        ...prevState,
+        notes: [newNote, ...prevState.notes],
+      };
+    });
+  }
+
   return (
     <div className="notes-list">
-      <Note></Note>
-      <Note></Note>
+      <Note input={true} onAdd={handleAddNote}></Note>
+      {notesState.notes.map((note) => (
+        <Note key={note.id} content={note.noteContent} />
+      ))}
     </div>
   );
 }
