cfa4c3f9c827cb26ef81495358cf668a32d88bae
dimovv23 <ggdimovv@gmail.com>
2024-02-09 23:37:10 +0200

    Thme change

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import NotesList from "./components/NotesList";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <div className="header">
         <h1>Notes</h1>
       </div>
       <NotesList />
-    </>
+    </div>
   );
 }
 
